143353043fce4a2e9666394babaed4aa78f2eb11
SherryH <enjoy.wind7rain@gmail.com>
2024-09-11 22:45:02 +0800

    Enabled showing each journalEntry

---
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,42 @@
+// this page.tsx is under [id] folder, so {params.id} returns the corresponding id
+
+// Editor is a client component that accepts an "entry" prop
+// This [id]/page.tsx is a server component that will get entry data from db
+
+import Editor from '@/components/Editor'
+import { getUserFromClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+// the journal entry is auto created when creating a new Entry, and placed on the url
+// The journal entry is uniquely identified by the userID + id
+// https://www.prisma.io/docs/orm/prisma-client/special-fields-and-types/working-with-composite-ids-and-constraints
+// So the JournalEntry schema needs to be uniquely identified   @@unique([userId,id])
+const getEntry = async (id: string) => {
+  const user = await getUserFromClerkId()
+  const journalEntry = await prisma.journalEntry.findUniqueOrThrow({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+  return journalEntry
+}
+
+// Server component can pass props to Client component, Editor
+// as long as the prop is serialisable
+type EntryPageType = {
+  id: string
+}
+
+const EntryPage = async ({ params }: { params: EntryPageType }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div>
+      <Editor entry={entry} />
+    </div>
+  )
+}
+
+export default EntryPage
---
+++ components/Editor.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { JournalEntry } from '@prisma/client'
+
+const Editor = ({ entry }: { entry: JournalEntry }) => {
+  return <div>{entry.content}</div>
+}
+
+export default Editor
