5a5c8f3e71585e8cd893f1a7b428efbad242615b
SherryH <enjoy.wind7rain@gmail.com>
2024-09-14 16:05:01 +0800

    Added autosave feature in the Editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,15 +2,26 @@
 import { updateEntry } from '@/utils/api'
 import { JournalEntry } from '@prisma/client'
 import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
 
 const Editor = ({ entry }: { entry: JournalEntry }) => {
   const [value, setValue] = useState(entry.content)
+  const [isLoading, setIsLoading] = useState(false)
   const handleClick = async () => {
     await updateEntry(entry.id, value)
   }
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      await updateEntry(entry.id, _value)
+      setIsLoading(false)
+    },
+  })
   return (
     <div className="h-full w-full">
       <button onClick={handleClick}>Save me</button>
+      {isLoading && <div>...isLoading</div>}
       <textarea
         className="h-full w-full outline-none"
         value={value}
