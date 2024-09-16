654cdeb31da1ca5ab23186d2cb1776560988a80c
SherryH <enjoy.wind7rain@gmail.com>
2024-09-16 16:12:54 +0800

    Added a navigation bar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,26 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' },
+]
 
 // The sharable components on the Dashboard like Navbar will stay static. No flashing issue
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-screen h-screen relative">
       <aside className="absolute top-0 left-0 h-full border-t border-r border-black/10 w-[200px]">
-        Mood Dashboard Layout
+        <div className="px-4 my-4">Mood Dashboard Layout</div>
+        <div>
+          <ul className="px-4">
+            {links.map((link) => (
+              <li key={link.label} className="text-xl my-4">
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-t border-b border-black/10">
