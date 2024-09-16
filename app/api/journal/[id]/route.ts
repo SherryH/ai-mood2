0451996d03eed7572688df0b7f741db9e4540667
SherryH <enjoy.wind7rain@gmail.com>
2024-09-16 12:45:40 +0800

    Enabled Analysis update with entry update

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -2,8 +2,10 @@
 // PUT replaces the whole thing in the DB
 // PATCH updates the differences
 
+import { analyse } from '@/utils/ai'
 import { getUserFromClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
 
 type ParamType = {
@@ -26,5 +28,23 @@ export const PATCH = async (request: Request, { params }: ParamType) => {
       content,
     },
   })
-  return NextResponse.json({ data: updatedJournalEntry })
+  const analysis = await analyse(content)
+  // Update just updates existing entry's analysis. If an entry doesn't have analysis, it may fail
+  // upset = update + insert. It updates an existing analysis. if an entry doesn't have an analysis, it will insert one
+  const savedAnalysis = await prisma.analysis.upsert({
+    where: {
+      entryId: updatedJournalEntry.id,
+    },
+    create: {
+      ...analysis!,
+      entryId: updatedJournalEntry.id,
+    },
+    update: {
+      ...analysis,
+    },
+  })
+
+  return NextResponse.json({
+    data: { ...updatedJournalEntry, analysis: savedAnalysis },
+  })
 }
